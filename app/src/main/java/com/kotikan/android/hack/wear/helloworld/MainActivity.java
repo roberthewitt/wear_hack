5d480525454dfca635556207ab1f2463302b5d06
Robert Hewitt <robert@kotikan.com>
2015-03-26 10:50:34 +0000

    player block jumps and rotates

--- app/src/main/java/com/kotikan/android/hack/wear/helloworld/MainActivity.java
+++ app/src/main/java/com/kotikan/android/hack/wear/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 
 import com.kotikan.android.hack.wear.helloworld.eventbus.Bus;
 import com.kotikan.android.hack.wear.helloworld.eventbus.EventHandler;
@@ -18,13 +19,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Bus.bus().register(new RotatePlayerListener((Context) this), OnScreenClicked.class);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnClickListener(new ScreenClickSender());
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-//                mTextView = (TextView) stub.findViewById(R.id.text);
+                final View playerBlock = stub.findViewById(R.id.player_block);
+                Bus.bus().register(new RotatePlayerListener(playerBlock), OnScreenClicked.class);
             }
         });
     }
--- app/src/main/java/com/kotikan/android/hack/wear/helloworld/eventbus/handlers/RotatePlayerListener.java
+++ app/src/main/java/com/kotikan/android/hack/wear/helloworld/eventbus/handlers/RotatePlayerListener.java
@@ -1,21 +1,46 @@
 package com.kotikan.android.hack.wear.helloworld.eventbus.handlers;
 
-import android.content.Context;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
 
 import com.kotikan.android.hack.wear.helloworld.eventbus.EventHandler;
 import com.kotikan.android.hack.wear.helloworld.eventbus.events.Event;
 
 public class RotatePlayerListener implements EventHandler {
 
-    private final Context context;
+    private final View playerBlock;
+    boolean alreadyAnimating = false;
 
-    public RotatePlayerListener(Context context) {
-        this.context = context;
+    public RotatePlayerListener(View playerBlock) {
+        this.playerBlock = playerBlock;
     }
 
     @Override
     public void handleEvent(Object o, Class<? extends Event> event) {
-        Toast.makeText(context, "screen clicked", Toast.LENGTH_SHORT).show();
+        if (!alreadyAnimating) {
+            alreadyAnimating = true;
+            ViewPropertyAnimator animate = playerBlock.animate();
+            animate.rotationBy(180f);
+            animate.setDuration(500);
+            animate.withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    alreadyAnimating = false;
+                }
+            });
+            animate.start();
+
+
+            ViewPropertyAnimator upAnimator = playerBlock.animate();
+            upAnimator.translationYBy(-100);
+            upAnimator.setDuration(250);
+            upAnimator.withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    playerBlock.animate().translationYBy(100).setDuration(250).start();
+                }
+            });
+            upAnimator.start();
+        }
     }
 }
